7473db437a3c5ee7ffa17e2e658c20cf53752b14
tristanlatr <tristanlatr@users.noreply.github.com>
2023-11-01 21:54:14 -0400

    - Simply use fnmatch patter for -k flag. - Add a few projects - Fix bisect mode not using asyncio.Task() explicitely

--- pydoctor_primer/projects.py
+++ pydoctor_primer/projects.py
@@ -1,49 +1,6 @@
 from __future__ import annotations
 
-import difflib
-import subprocess
-import sys
-
 from pydoctor_primer.model import Project
-
-def update_projects(projects: list[Project], check: bool = False) -> None:
-    # modifies `get_projects` in place.
-    result = []
-    with open(__file__) as f:
-        keep = True
-        for line in f:
-            if line.endswith("\n"):
-                line = line[:-1]
-            if line == "    projects = [":
-                result.append(f"    projects = {projects!r}")
-                keep = False
-            if keep:
-                result.append(line)
-            if line == "    ]":
-                keep = True
-
-    if check:
-        code_proc = subprocess.run(
-            ["black", "-"], input="\n".join(result), capture_output=True, text=True
-        )
-        code_proc.check_returncode()
-        code = code_proc.stdout
-
-        with open(__file__) as f:
-            in_file = f.read()
-            if in_file != code:
-                diff = difflib.context_diff(
-                    in_file.splitlines(keepends=True),
-                    code.splitlines(keepends=True),
-                    fromfile=__file__,
-                    tofile=__file__,
-                )
-                print("".join(diff))
-                sys.exit(1)
-    else:
-        with open(__file__, "w") as f:
-            f.write("\n".join(result))
-
 
 def get_projects() -> list[Project]:
     projects = [
@@ -54,8 +11,22 @@ def get_projects() -> list[Project]:
         ),
         Project(
             location="https://github.com/twisted/twisted",
-            pydoctor_cmd="{pydoctor} ./src/twisted --docformat=plaintext", 
+            pydoctor_cmd="{pydoctor} ./src/twisted", 
+            expected_success=False
+        ),
+        Project(
+            location="https://github.com/twisted/tubes",
+            pydoctor_cmd=("{pydoctor} ./tubes --privacy='HIDDEN:tubes.test' "
+                          "--intersphinx=https://docs.python.org/3/objects.inv "
+                          "--intersphinx=https://docs.twisted.org/en/stable/api/objects.inv "
+                          "--intersphinx=https://zopeinterface.readthedocs.io/en/latest/objects.inv "), 
+            revision='v0.2.1',
             expected_success=True
+        ),
+        Project(
+            location="https://github.com/temporalio/sdk-python",
+            pydoctor_cmd=("{pydoctor} --project-base-dir=."), 
+            expected_success=False
         ),
     ]
     assert len(projects) == len({p.name for p in projects})
